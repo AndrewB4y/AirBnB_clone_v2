2044791324240da38dc8c69e12f21fa9cdcaa0d9
AndrewB4y <andresbaymon@gmail.com>
2020-08-31 22:38:00 +0000

    Adding file 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+
+""" 0-hello_route module """
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ hello route method """
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
